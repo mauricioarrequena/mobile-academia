26a42ff155b9970937542dc7e05ebd02814579de
mauricioarroyoas <mauricio.arroyo@assuresoft.tech>
2025-06-17 17:10:45 -0400

    feat: restructued code (#18)
    * feat: added dependencies
    
    * feat: restructued code

--- App.tsx
+++ App.tsx
@@ -1,34 +1,17 @@
-// App.tsx
 import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
 import {StatusBar, StyleSheet, useColorScheme} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-// Importing the custom TabBar component
-import TabBar from './src/components/TabBar';
-// Importing your screen components
-import Home from './src/screens/HomeScreen';
-import Search from './src/screens/SearchScreen';
-import WishlistScreen from './src/screens/WishlistScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
 
 const styles = StyleSheet.create({
-  app: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-  },
+  app: {flex: 1, display: 'flex', flexDirection: 'column'},
 });
 
 function App(): React.JSX.Element {
   const colorScheme = useColorScheme();
   const isDarkMode = colorScheme === 'dark';
+
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={styles.app}>
@@ -37,44 +20,7 @@ function App(): React.JSX.Element {
           backgroundColor="transparent"
           barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         />
-        <NavigationContainer>
-          <Tab.Navigator
-            tabBar={props => <TabBar {...props} />}
-            screenOptions={{
-              headerShown: false, // This disables the header on all tabs
-            }}>
-            <Tab.Screen
-              name="Home"
-              component={Home}
-              options={() => ({
-                tabBarLabel: 'Home',
-                // Custom value passed via route.params
-                // Your TabBar component will read this from route.name or hardcoded mapping
-              })}
-            />
-            <Tab.Screen
-              name="Search"
-              component={Search}
-              options={() => ({
-                tabBarLabel: 'Search',
-              })}
-            />
-            <Tab.Screen
-              name="Wishlist"
-              component={WishlistScreen}
-              options={() => ({
-                tabBarLabel: 'Wishlist',
-              })}
-            />
-            <Tab.Screen
-              name="Profile"
-              component={ProfileScreen}
-              options={() => ({
-                tabBarLabel: 'Profile',
-              })}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <AppNavigator />
       </GestureHandlerRootView>
     </SafeAreaProvider>
   );
---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,47 @@
+// src/navigation/AppNavigator.tsx
+import React from 'react';
+import {
+  BottomTabBarProps,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import TabBar from '../components/TabBar';
+import Home from '../screens/HomeScreen';
+import Search from '../screens/SearchScreen';
+import WishlistScreen from '../screens/WishlistScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+export default function AppNavigator() {
+  const Tab = createBottomTabNavigator();
+
+  function renderTabBar(props: BottomTabBarProps) {
+    return <TabBar {...props} />;
+  }
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator tabBar={renderTabBar} screenOptions={{headerShown: false}}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{tabBarLabel: 'Home'}}
+        />
+        <Tab.Screen
+          name="Search"
+          component={Search}
+          options={{tabBarLabel: 'Search'}}
+        />
+        <Tab.Screen
+          name="Wishlist"
+          component={WishlistScreen}
+          options={{tabBarLabel: 'Wishlist'}}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{tabBarLabel: 'Profile'}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
