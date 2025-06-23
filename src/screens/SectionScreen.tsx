31fc5e37936b4b02ff48db3cfcb5aeb5f1ee2fd9
mauricioarrequena <mauricio.ar.requena@gmail.com>
2025-06-23 17:18:37 -0400

    feat: added dark mode support

--- src/screens/SectionScreen.tsx
+++ src/screens/SectionScreen.tsx
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Dimensions,
   Image,
+  useColorScheme,
 } from 'react-native';
 import useTMDB from '../hooks/useTMDB';
 import {TMDB_IMAGES_BASE_URL} from '@env';
@@ -22,88 +23,86 @@ const SPACE = 10;
 const CARD_WIDTH =
   Dimensions.get('window').width / NUMBER_OF_COLUMNS - SPACE * 2;
 
-const styles = StyleSheet.create({
-  sectionScreen: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    paddingTop: 20,
-    rowGap: 10,
-    borderWidth: 2,
-    borderColor: 'yellow',
-  },
-  sectionScreenHeader: {
-    alignSelf: 'flex-start',
-    marginLeft: SPACE,
-    display: 'flex',
-    flexDirection: 'row',
-    // borderWidth: 2,
-    // borderColor: 'green',
-  },
-  listContainer: {
-    rowGap: SPACE * 2,
-    paddingBottom: 30,
-    borderWidth: 2,
-    borderColor: 'red',
-  },
-  card: {
-    marginRight: SPACE / 1.5,
-    width: CARD_WIDTH,
-    height: 370,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    gap: 8,
-    backgroundColor: '#fff',
-    borderRadius: 15,
-    overflow: 'hidden',
-    borderWidth: 3,
-    borderColor: '#E0E0E0', // Material light outline
-  },
-  cardText: {
-    fontSize: 16,
-    // borderWidth: 2,
-    // borderColor: 'orange',
-  },
-  movieItem: {
-    width: 100,
-    alignItems: 'center',
-    gap: 8,
-    // borderWidth: 2,
-    // borderColor: 'brown',
-  },
-  image: {
-    flexBasis: '90%',
-    width: '100%',
-    overflow: 'hidden',
-    // borderWidth: 2,
-    // borderColor: 'pink',
-  },
-  textContainer: {
-    flex: 1,
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    // borderWidth: 2,
-    // borderColor: 'blue',
-  },
-  text: {
-    textAlign: 'center',
-    // padding: 4,
-    color: 'black',
-    // borderWidth: 2,
-    // borderColor: 'teal',
-  },
-});
+const getStyles = (isDarkMode: boolean) =>
+  StyleSheet.create({
+    sectionScreen: {
+      flex: 1,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      paddingTop: 20,
+      rowGap: 10,
+      backgroundColor: isDarkMode ? '#121212' : '#FFFFFF',
+      // borderWidth: 2,
+      // borderColor: 'yellow',
+    },
+    sectionScreenHeader: {
+      alignSelf: 'flex-start',
+      marginLeft: SPACE,
+      display: 'flex',
+      flexDirection: 'row',
+      // borderWidth: 2,
+      // borderColor: 'green',
+    },
+    listContainer: {
+      rowGap: 20,
+      paddingBottom: 30,
+      // borderWidth: 2,
+      // borderColor: 'red',
+    },
+    card: {
+      height: 370,
+      width: CARD_WIDTH,
+      marginRight: SPACE / 1.5,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      overflow: 'hidden',
+      backgroundColor: isDarkMode ? '#1E2A38' : '#fff',
+      borderRadius: 10,
+      borderWidth: 3,
+      borderColor: isDarkMode ? '#2E3C4D' : '#E0E0E0',
+    },
+    image: {
+      flexBasis: '90%',
+      width: '100%',
+      overflow: 'hidden',
+      // borderWidth: 2,
+      // borderColor: 'pink',
+    },
+    contentContainer: {
+      flex: 1,
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      // borderWidth: 2,
+      // borderColor: 'blue',
+    },
+    text: {
+      textAlign: 'center',
+      color: isDarkMode ? '#E0E0E0' : '#212121',
+    },
+    textLarge: {
+      fontSize: 20,
+    },
+    textSmall: {
+      fontSize: 16,
+    },
+    textBold: {
+      fontWeight: 'bold',
+    },
+  });
 
 export default function SectionScreen() {
   const route = useRoute<RouteProp<RootStackParamList, 'SectionScreen'>>();
   const {categoryName, endpoint, params} = route.params ?? {};
   const {movies} = useTMDB(endpoint, params);
+  const colorScheme = useColorScheme();
+  const isDarkMode = colorScheme === 'dark';
+  const styles = getStyles(isDarkMode);
 
   const renderItem = ({item}: {item: PopularMovie}) => (
     <View style={styles.card}>
@@ -113,8 +112,10 @@ export default function SectionScreen() {
         }}
         style={styles.image}
       />
-      <View style={styles.textContainer}>
-        <Text style={styles.text} numberOfLines={1}>
+      <View style={styles.contentContainer}>
+        <Text
+          style={[styles.text, styles.textBold, styles.textSmall]}
+          numberOfLines={1}>
           {item.title ? item.title : item.name}
         </Text>
       </View>
@@ -125,11 +126,11 @@ export default function SectionScreen() {
     <View style={styles.sectionScreen}>
       <Text style={styles.sectionScreenHeader}>{categoryName}</Text>
       <FlatList
+        contentContainerStyle={styles.listContainer}
         data={movies}
         renderItem={renderItem}
         keyExtractor={(item, index) => index.toString()}
         numColumns={NUMBER_OF_COLUMNS}
-        contentContainerStyle={styles.listContainer}
       />
     </View>
   );
