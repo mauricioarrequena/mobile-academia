9a14ca9f466447745444bfa003137ec987c650dd
Miguel Angel Gonzales Melgar <miguelangel.gonzales@assuresoft.tech>
2025-06-26 19:24:13 -0400

    chore: create a new hook to fetch movies by id, adjust the type to add movie details.

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,8 +1,15 @@
 export interface Movie {
   id: number;
-  overview: string;
+  overview?: string;
   release_date: string;
   title: string;
   poster_path: string;
   name: string;
+}
+
+export interface MovieDetail extends Movie {
+  backdrop_path?: string;
+  genres?: {id: number; name: string}[];
+  runtime?: number;
+  vote_average?: number;
 }
