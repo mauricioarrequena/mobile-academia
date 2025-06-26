6610ce8a90fe9ea02ee5ac60ac283075392b8140
Mauricio Arroyo <mauricio.arroyo@assuresoft.tech>
2025-06-26 15:36:32 -0400

    feat: added CollectionList.tsx and CollectionListItem.tsx components

--- src/components/CollectionList.tsx
+++ src/components/CollectionList.tsx
@@ -1,117 +1,66 @@
-import { useEffect, useState } from 'react';
 import {
   View,
   Text,
   FlatList,
   StyleSheet,
   ActivityIndicator,
-  Pressable,
 } from 'react-native';
-import { Collection } from '../types/Collection';
-import { getDBConnection } from '../database/db';
+import {Collection} from '../types/Collection';
+import CollectionListItem from './CollectionListItem';
 
 const styles = StyleSheet.create({
   collectionList: {
     flex: 1,
-    padding: 16,
   },
-  title: {
-    marginBottom: 12,
-    fontSize: 20,
-    fontWeight: '700',
-    color: '#222',
+  activityIndicator: {
+    marginTop: 20,
   },
-  listItem: {
-    paddingVertical: 16,
-    paddingHorizontal: 12,
-    borderBottomWidth: 1,
-    borderBottomColor: '#e0e0e0',
-    backgroundColor: '#fff',
-  },
-  listItemPressed: {
-    backgroundColor: '#f1f1f1',
-  },
-  name: {
-    fontSize: 16,
-    fontWeight: '600',
-    color: '#000',
-  },
-  date: {
-    fontSize: 12,
-    color: '#666',
-    marginTop: 4,
-  },
+
   emptyText: {
     fontStyle: 'italic',
     color: '#888',
     marginTop: 12,
   },
-  activityIndicator: {
-    marginTop: 20,
+  list: {
+    gap: 10,
   },
 });
 
-export default function CollectionList() {
-  const [collections, setCollections] = useState<Collection[]>([]);
-  const [loading, setLoading] = useState(true);
+interface CollectionListProps {
+  collections: Collection[];
+  collectionsLoading: boolean;
+  reloadCollections: () => void;
+}
 
-  useEffect(() => {
-    loadCollections();
-  }, []);
+export default function CollectionList({
+  collections,
+  collectionsLoading,
+}: CollectionListProps) {
+  let TextNoCollections;
+  let FlastListCollections;
 
-  const loadCollections = async () => {
-    try {
-      const database = await getDBConnection();
-      const results = await database.executeSql('SELECT * FROM Collections');
-      const rawRows = results[0].rows;
-      const collectionItem: Collection[] = [];
-      for (let i = 0; i < rawRows.length; i++) {
-        collectionItem.push(rawRows.item(i));
-      }
-      setCollections(collectionItem);
-      // // Fake loading delay of 2 seconds to show spinner
-      // await new Promise(resolve => setTimeout(resolve, 3000));
-    } catch (error) {
-      console.error('Error loading collections:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  if (loading) {
+  if (collectionsLoading) {
     return <ActivityIndicator size="large" style={styles.activityIndicator} />;
+  }
+
+  if (collections.length === 0) {
+    TextNoCollections = (
+      <Text style={styles.emptyText}>No collections found.</Text>
+    );
+  } else {
+    FlastListCollections = (
+      <FlatList
+        data={collections}
+        keyExtractor={item => item.id.toString()}
+        contentContainerStyle={styles.list}
+        renderItem={({item}) => <CollectionListItem colelction={item} />}
+      />
+    );
   }
 
   return (
     <View style={styles.collectionList}>
-      <Text style={styles.title}>My Collections</Text>
-      {collections.length === 0 ? (
-        <Text style={styles.emptyText}>No collections found.</Text>
-      ) : (
-        <FlatList
-          data={collections}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <Pressable
-              android_ripple={{ color: '#ddd' }}
-              style={({ pressed }) => [
-                styles.listItem,
-                pressed && styles.listItemPressed,
-              ]}
-              onPress={() => {
-                /* You can add item click behavior here */
-              }}
-            >
-              <View>
-                <Text style={styles.name}>{item.name}</Text>
-                <Text style={styles.date}>
-                  Created: {new Date(item.created_at).toLocaleDateString()}
-                </Text>
-              </View>
-            </Pressable>
-          )}
-        />
-      )}
+      {collections.length === 0 ? TextNoCollections : FlastListCollections}
     </View>
   );
 }
