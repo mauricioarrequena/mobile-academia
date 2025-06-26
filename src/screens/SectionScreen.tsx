df3701b08691b67d8db10d5a7ca2af6bce46373c
sebastianAssure <sebastian.padilla@assuresoft.tech>
2025-06-26 18:36:30 -0400

    ref: modify style of movie-card on screens (#32)
    Co-authored-by: Sebastian Padilla <sebastianpadilla@ascbomacmini80.local>

--- src/screens/SectionScreen.tsx
+++ src/screens/SectionScreen.tsx
@@ -1,98 +1,33 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  FlatList,
-  StyleSheet,
-  Dimensions,
-  Image,
-  useColorScheme,
-} from 'react-native';
+import {View, Text, FlatList, StyleSheet, useColorScheme} from 'react-native';
 import useTMDB from '../hooks/useTMDB';
-import {TMDB_IMAGES_BASE_URL} from '@env';
 import {useRoute, RouteProp} from '@react-navigation/native';
-import { Movie } from '../types/Movie';
+import MovieCard from '../components/MovieCard';
 
 type RootStackParamList = {
   Tabs: undefined;
   SectionScreen: {categoryName: string; endpoint: string; params: any};
 };
 
-const NUMBER_OF_COLUMNS = 2;
-const SPACE = 10;
-const CARD_WIDTH =
-  Dimensions.get('window').width / NUMBER_OF_COLUMNS - SPACE * 2;
+const NUMBER_OF_COLUMNS = 3;
 
 const getStyles = (isDarkMode: boolean) =>
   StyleSheet.create({
     sectionScreen: {
       flex: 1,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      paddingTop: 20,
-      rowGap: 10,
-      backgroundColor: isDarkMode ? '#121212' : '#FFFFFF',
-      // borderWidth: 2,
-      // borderColor: 'yellow',
+      paddingTop: 60,
+      paddingHorizontal: 16,
+      backgroundColor: isDarkMode ? 'black' : '#FFFFFF',
     },
-    sectionScreenHeader: {
-      alignSelf: 'flex-start',
-      marginLeft: SPACE,
-      display: 'flex',
-      flexDirection: 'row',
-      // borderWidth: 2,
-      // borderColor: 'green',
-    },
-    listContainer: {
-      rowGap: 20,
-      paddingBottom: 30,
-      // borderWidth: 2,
-      // borderColor: 'red',
-    },
-    card: {
-      height: 370,
-      width: CARD_WIDTH,
-      marginRight: SPACE / 1.5,
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'flex-start',
-      alignItems: 'center',
-      overflow: 'hidden',
-      backgroundColor: isDarkMode ? '#1E2A38' : '#fff',
-      borderRadius: 10,
-      borderWidth: 3,
-      borderColor: isDarkMode ? '#2E3C4D' : '#E0E0E0',
-    },
-    image: {
-      flexBasis: '90%',
-      width: '100%',
-      overflow: 'hidden',
-      // borderWidth: 2,
-      // borderColor: 'pink',
-    },
-    contentContainer: {
-      flex: 1,
-      width: '100%',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      // borderWidth: 2,
-      // borderColor: 'blue',
-    },
-    text: {
-      textAlign: 'center',
-      color: isDarkMode ? '#E0E0E0' : '#212121',
-    },
-    textLarge: {
-      fontSize: 20,
-    },
-    textSmall: {
-      fontSize: 16,
-    },
-    textBold: {
+    sectionTitle: {
+      color: isDarkMode ? '#FFFFFF' : 'black',
+      fontSize: 24,
       fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    columnWrapperStyle: {
+      justifyContent: 'space-between',
+      marginBottom: 16,
     },
   });
 
@@ -104,33 +39,18 @@ export default function SectionScreen() {
   const isDarkMode = colorScheme === 'dark';
   const styles = getStyles(isDarkMode);
 
-  const renderItem = ({item}: {item: Movie}) => (
-    <View style={styles.card}>
-      <Image
-        source={{
-          uri: `${TMDB_IMAGES_BASE_URL}/w185${item.poster_path}`,
-        }}
-        style={styles.image}
-      />
-      <View style={styles.contentContainer}>
-        <Text
-          style={[styles.text, styles.textBold, styles.textSmall]}
-          numberOfLines={1}>
-          {item.title ? item.title : item.name}
-        </Text>
-      </View>
-    </View>
-  );
-
   return (
     <View style={styles.sectionScreen}>
-      <Text style={styles.sectionScreenHeader}>{categoryName}</Text>
+      <Text style={styles.sectionTitle}>{categoryName}</Text>
       <FlatList
-        contentContainerStyle={styles.listContainer}
         data={movies}
-        renderItem={renderItem}
-        keyExtractor={(item, index) => index.toString()}
+        keyExtractor={item => item.id.toString()}
         numColumns={NUMBER_OF_COLUMNS}
+        showsVerticalScrollIndicator={false}
+        columnWrapperStyle={styles.columnWrapperStyle}
+        renderItem={({item}) => (
+          <MovieCard movie={item} showTitle={true} isWishListScreen={false} />
+        )}
       />
     </View>
   );
