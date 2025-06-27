083d75f36b548275f9ade8d216275b8e420ee058
Mauricio Arroyo <mauricio.arroyo@assuresoft.tech>
2025-06-27 11:19:50 -0400

    feature: now it is possible to go to deatils screen movie

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -3,7 +3,6 @@ import {
   BottomTabBarProps,
   createBottomTabNavigator,
 } from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
 import TabBarFooter from '../components/TabBarFooter';
 import HomeStackNavigator from './HomeStackNavigator';
 import SearchScreen from '../screens/SearchScreen';
@@ -25,34 +24,30 @@ const renderTabBarFooter = (props: BottomTabBarProps) => {
 
 const MainNavigator = () => {
   return (
-    <NavigationContainer>
-      <TabNavigator.Navigator
-        tabBar={renderTabBarFooter}
-        screenOptions={{headerShown: false}}>
-        <TabNavigator.Screen
-          name="HomeTab"
-          component={HomeStackNavigator}
-          options={{
-            title: 'Home',
-          }}
-        />
-        <TabNavigator.Screen
-          name="SearchScreen"
-          component={SearchScreen}
-          options={{title: 'Search'}}
-        />
-        <TabNavigator.Screen
-          name="Wishlist"
-          component={WishlistScreen}
-          options={{title: 'Wishlist'}}
-        />
-        <TabNavigator.Screen
-          name="ProfileTab"
-          component={ProfileStackNavigator}
-          options={{title: 'Profile',}}
-        />
-      </TabNavigator.Navigator>
-    </NavigationContainer>
+    <TabNavigator.Navigator
+      tabBar={renderTabBarFooter}
+      screenOptions={{headerShown: false}}>
+      <TabNavigator.Screen
+        name="HomeTab"
+        component={HomeStackNavigator}
+        options={{title: 'Home'}}
+      />
+      <TabNavigator.Screen
+        name="SearchScreen"
+        component={SearchScreen}
+        options={{title: 'Search'}}
+      />
+      <TabNavigator.Screen
+        name="Wishlist"
+        component={WishlistScreen}
+        options={{title: 'Wishlist'}}
+      />
+      <TabNavigator.Screen
+        name="ProfileTab"
+        component={ProfileStackNavigator}
+        options={{title: 'Profile'}}
+      />
+    </TabNavigator.Navigator>
   );
 };
 
